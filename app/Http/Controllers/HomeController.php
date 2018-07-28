e65fcd3d9caa5f22078d2ce894b189730cdfc166
maulik <mvp46@njit.edu>
2018-07-28 17:33:17 -0400

    15. Deploying app on heroku

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -1,7 +1,9 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+
 class HomeController extends Controller
 {
     /**
@@ -13,6 +15,7 @@ class HomeController extends Controller
     {
         $this->middleware('auth');
     }
+
     /**
      * Show the application dashboard.
      *
@@ -20,8 +23,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        $user = Auth::user();
-        $questions = $user->questions()->paginate(6);
-        return view('home')->with('questions', $questions);
+        return view('home');
     }
 }
